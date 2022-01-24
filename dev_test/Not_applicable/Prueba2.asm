9b353a91786cd2fc1951d76a07cf01158b0fda04
Peter Yau <pty268@hotmail.com>
2022-01-24 17:00:29 -0600

    Copiado de worksheets
    Se agregó el código de copiado de worksheets de los archivos .xls* que se crean hacia el Master.xlsx del directorio.

---
+++ dev_test/Not_applicable/Prueba2.asm
@@ -0,0 +1,398 @@
+;*******************************************************************************
+;                                                                              *
+;    Filename:	    Proyecto Final                                             *
+;    Date:          11/11/18                                                   *
+;    File Version:                                                             *
+;    Author:        Peter Yau 17914, Daniel Cano 17272                         *
+;    Company:       UVG                                                        *
+;    Description;*******************************************************************************:                                                              *
+;                                                                              *
+;*******************************************************************************
+; TODO Step #1 Processor Inclusion
+;*******************************************************************************
+
+#include "p16f887.inc"
+
+;*******************************************************************************
+; TODO Step #2 - Configuration Word Setup
+;*******************************************************************************
+
+; CONFIG1
+; __config 0xFFD4
+ __CONFIG _CONFIG1, _FOSC_INTRC_NOCLKOUT & _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF & _CP_OFF & _CPD_OFF & _BOREN_OFF & _IESO_OFF & _FCMEN_OFF & _LVP_OFF
+; CONFIG2
+; __config 0xFFFF
+ __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
+
+;*******************************************************************************
+; TODO Step #3 - Variable Definitions
+;*******************************************************************************
+
+GPR_VAR	UDATA
+STATUS_TEMP RES 1
+W_TEMP	    RES 1
+CONT1	    RES 1
+CONT2	    RES 1
+CONVERSION  RES 1
+CONTADOR    RES 1
+POT3	    RES 1
+POT4	    RES 1
+	    
+;*******************************************************************************
+; Reset Vector
+;*******************************************************************************
+
+RES_VECT  CODE    0x0000            ; processor reset vector
+    GOTO    SETUP                    ; go to beginning of program
+
+;*******************************************************************************
+; TODO Step #4 - Interrupt Service Routines
+;*******************************************************************************
+
+ISR_VECT  CODE    0x0004
+PUSH:
+    BCF	    INTCON,0
+    BCF	    INTCON,1
+    MOVWF   W_TEMP
+    SWAPF   STATUS,W
+    MOVWF   STATUS_TEMP
+ISR:
+    BCF	    INTCON,T0IF
+    MOVLW   .248
+    MOVWF   TMR0
+    INCF    CONTADOR
+    RRF	    POT3,F
+    RRF	    POT3,F
+    RRF	    POT3,W
+    ANDLW   B'00011111'
+    SUBWF   CONTADOR,W
+    BTFSC   STATUS,C
+    BSF	    PORTD,RD1
+    BTFSS   STATUS,C
+    BCF	    PORTD,RD1
+    
+    RRF	    POT4,F
+    RRF	    POT4,F
+    RRF	    POT4,W
+    ANDLW   B'00011111'
+    SUBWF   CONTADOR,W
+    BTFSC   STATUS,C
+    BSF	    PORTD,RD2
+    BTFSS   STATUS,C
+    BCF	    PORTD,RD2
+    
+POP2:
+    SWAPF   STATUS_TEMP,W
+    MOVWF   STATUS
+    SWAPF   W_TEMP,F
+    SWAPF   W_TEMP,W
+    BSF	    INTCON,0
+    BSF	    INTCON,1
+    RETFIE			    ; RETORNO DE INTERRUPCIÛN
+    
+;*******************************************************************************
+; MAIN PROGRAM
+;*******************************************************************************
+
+MAIN_PROG CODE                      ; let linker place main program
+
+SETUP
+    CALL    CONFIG_IO
+    CALL    CONFIG_OSC
+    CALL    CONFIG_INT
+    CALL    CONFIG_ADC
+    CALL    CONFIG_PWM1
+    CALL    CONFIG_PWM2
+    CALL    CONFIG_RXTX
+    CLRF    CONTADOR
+    GOTO    PWM1
+    
+PWM1
+    CALL    SET_AN0	    ; SET AN0 CHANNEL
+    CALL    DELAY
+    BSF	    ADCON0,GO
+CHECKADC:
+    BTFSC   ADCON0,GO	    ; VERIFICA SI CONVERSION YA FINALIZÛ
+    GOTO    CHECKADC	    ; NO? VOLVER A CHEQUEAR
+    MOVF    ADRESH,W	    
+    MOVWF   CONVERSION	    ; MOVER ADRESH A W Y LUEGO A VARIABLE CONVERSION
+    BCF	    PIR1,ADIF	    ; VACIAR FLAG DE ADC
+CHECK_TX:
+    BTFSS   PIR1,TXIF
+    GOTO    CHECK_TX
+;    RRF	    CONVERSION,F
+;    RRF	    CONVERSION,W
+    MOVF    CONVERSION,W
+    MOVWF   TXREG
+    BTFSS   PIR1,TXIF
+    GOTO    $-1
+CHECK_RX:
+    BTFSS   PIR1,RCIF	    ; VERIFICAR FLAG DE RECIBIDO
+    GOTO    CHECK_RX	    ; FLAG NO SET? VERIFICAR EL TX
+;    MOVF    RCREG,W
+    RRF	    RCREG,F
+    RRF	    RCREG,F
+    RRF	    RCREG,W
+    MOVWF   PORTB
+    MOVF    PORTB,W
+    ANDLW   B'00011111'
+    MOVWF   CCPR1L
+    
+PWM2
+    CALL    SET_AN1
+    CALL    DELAY
+    BSF	    ADCON0,GO
+CHECKADC2:
+    BTFSC   ADCON0,GO
+    GOTO    CHECKADC2
+    MOVF    ADRESH,W
+    MOVWF   CONVERSION
+    BCF	    PIR1,ADIF
+CHECK_TX2:
+    BTFSS   PIR1,TXIF
+    GOTO    CHECK_TX2
+    MOVF    CONVERSION,W
+    MOVWF   TXREG
+    BTFSS   PIR1,TXIF
+    GOTO    $-1
+CHECK_RX2:
+    BTFSS   PIR1,RCIF
+    GOTO    CHECK_RX2
+    RRF	    RCREG,F
+    RRF	    RCREG,F
+    RRF	    RCREG,W
+    ANDLW   B'00011111'
+    MOVWF   CCPR2L
+    
+PWM3
+    CALL    SET_AN2
+    CALL    DELAY
+    BSF	    ADCON0,GO
+CHECKADC3:
+    BTFSC   ADCON0,GO
+    GOTO    CHECKADC3
+    MOVF    ADRESH,W
+    MOVWF   POT3
+    BCF	    PIR1,ADIF
+CHECK_TX3:
+    BTFSS   PIR1,TXIF
+    GOTO    CHECK_TX3
+    MOVF    PORTD
+    
+PWM4
+    CALL    SET_AN3
+    CALL    DELAY
+    BSF	    ADCON0,GO
+CHECKADC4:
+    BTFSC   ADCON0,GO
+    GOTO    CHECKADC4
+    MOVF    ADRESH,W
+    MOVWF   POT4
+    BCF	    PIR1,ADIF
+    
+    
+    GOTO PWM1
+    
+;*******************************************************************************
+; SUBROUTINES
+;******************************************************************************* 
+    
+CONFIG_IO
+    BANKSEL PORTA
+    CLRF    PORTA
+    CLRF    PORTB
+    CLRF    PORTC
+    CLRF    PORTD
+    BANKSEL TRISA
+    BSF	    TRISA,0	; RA0 INPUT
+    BSF	    TRISA,1	; RA1 INPUT
+    BSF	    TRISA,2	; RA2 INPUT
+    BSF	    TRISA,3	; RA3 INPUT
+    CLRF    TRISB
+    CLRF    TRISC
+    CLRF    TRISD
+    BANKSEL ANSEL
+    BSF	    ANSEL,0	; RA0 ANALÛGICO
+    BSF	    ANSEL,1	; RA1 ANALÛGICO
+    BSF	    ANSEL,2	; RA2 ANALÛGICO
+    BSF	    ANSEL,3	; RA3 ANALÛGICO
+    CLRF    ANSELH
+    RETURN
+    
+CONFIG_OSC
+    BANKSEL OSCCON
+    BCF	    OSCCON,IRCF2
+    BSF	    OSCCON,IRCF1
+    BSF	    OSCCON,IRCF0	; OSCILADOR A 500 KHZ
+    RETURN
+    
+CONFIG_INT
+    BANKSEL INTCON
+    BSF	    INTCON,GIE	    ; ENABLE GLOBAL INTERRUPTIONS
+    BCF	    INTCON,PEIE	    ; DISABLE PERIPHERAL INTERRUPTIONS
+    RETURN
+    
+CONFIG_ADC
+    BANKSEL ADCON0
+    BCF	    ADCON0,ADCS1
+    BSF	    ADCON0,ADCS0    ; FOSC/8
+    BCF	    ADCON0,CHS3
+    BCF	    ADCON0,CHS2
+    BCF	    ADCON0,CHS1
+    BCF	    ADCON0,CHS0	    ; AN0 CHANNEL
+    
+    BANKSEL ADCON1
+    BCF	    ADCON1,ADFM	    ; LEFT JUSTIFIED
+    BCF	    ADCON1,VCFG1    ; VDD INTERNO
+    BCF	    ADCON1,VCFG0    ; VSS INTERNO
+    
+    BANKSEL PIR1
+    BCF	    PIR1,ADIF
+    
+    BANKSEL ADCON0
+    BSF	    ADCON0,ADON
+    
+    RETURN
+    
+CONFIG_PWM1
+    BANKSEL PR2
+    MOVLW   .155
+    MOVWF   PR2   
+    
+    BANKSEL CCP1CON
+    BCF	    CCP1CON,P1M1
+    BCF	    CCP1CON,P1M0	; SINGLE OUTPUT
+    BSF	    CCP1CON,CCP1M3
+    BSF	    CCP1CON,CCP1M2
+    BCF	    CCP1CON,CCP1M1
+    BSF	    CCP1CON,CCP1M0	; PWM MODE, P1A,P1C ACTIVE-HIGH, P1B,P1D ACTIVE-LOW
+    
+    MOVLW   B'00001111'
+    MOVWF   CCPR1L
+    BSF	    CCP1CON, DC1B1
+    BCF	    CCP1CON, DC1B0	    ; LSB del duty cicle
+    
+    RETURN
+    
+CONFIG_PWM2
+    BANKSEL PR2
+    MOVLW   .155
+    MOVWF   PR2
+    
+    BANKSEL CCP2CON
+    BSF	    CCP2CON,CCP2M3
+    BSF	    CCP2CON,CCP2M2
+    BSF	    CCP2CON,CCP2M1
+    BSF	    CCP2CON,CCP2M0
+    
+    MOVLW   B'00001111'
+    MOVWF   CCPR2L
+    BSF	    CCP2CON, DC2B1
+    BCF	    CCP2CON, DC2B0	    ; LSB del duty cicle
+    
+    BANKSEL T2CON
+    MOVLW   B'00000111'
+    MOVWF   T2CON
+    BANKSEL PIR1
+    BCF     PIR1,TMR2IF
+    
+    RETURN
+    
+CONFIG_RXTX
+    BANKSEL BAUDCTL
+    BCF	    BAUDCTL,BRG16
+    
+    BANKSEL TXSTA
+    BCF	    TXSTA,TX9
+    BCF	    TXSTA,SYNC
+    BSF	    TXSTA,BRGH
+    BSF	    TXSTA,TXEN
+    
+    BANKSEL SPBRG
+    MOVLW   .12		; BAUDRATE 2400
+    MOVWF   SPBRG
+    CLRF    SPBRGH
+    
+    
+    BANKSEL RCSTA
+    BCF	    RCSTA,RX9
+    BSF	    RCSTA,CREN
+    BSF	    RCSTA,SPEN
+    
+    BANKSEL PIR1
+    BCF	    PIR1,RCIF
+    BCF	    PIR1,TXIF
+    
+    RETURN    
+    
+SET_AN0
+    BANKSEL ADCON0
+    BCF	    ADCON0,CHS3
+    BCF	    ADCON0,CHS2
+    BCF	    ADCON0,CHS1
+    BCF	    ADCON0,CHS0
+    RETURN
+    
+SET_AN1
+    BANKSEL ADCON0
+    BCF	    ADCON0,CHS3
+    BCF	    ADCON0,CHS2
+    BCF	    ADCON0,CHS1
+    BSF	    ADCON0,CHS0
+    RETURN
+    
+SET_AN2
+    BANKSEL ADCON0
+    BCF	    ADCON0,CHS3
+    BCF	    ADCON0,CHS2
+    BSF	    ADCON0,CHS1
+    BCF	    ADCON0,CHS0
+    RETURN
+    
+SET_AN3
+    BANKSEL ADCON0
+    BCF	    ADCON0,CHS3
+    BCF	    ADCON0,CHS2
+    BSF	    ADCON0,CHS1
+    BSF	    ADCON0,CHS0
+    RETURN
+    
+CONFIG_TMR0
+    BANKSEL OPTION_REG
+    BCF	    OPTION_REG,T0CS	; INTERNAL CYCLE CLOCK
+    BCF	    OPTION_REG,T0SE	; INCREMENT LOW-TO-HIGH
+    BCF	    OPTION_REG,PSA	; PRESCALES ASIGNADO A TMR0
+    
+    BCF	    OPTION_REG,PS2
+    BSF	    OPTION_REG,PS1
+    BSF	    OPTION_REG,PS0	; PRESCALER 
+
+    BANKSEL INTCON
+    BSF	    INTCON,T0IE		; ENABLE TMR0 
+    
+    BANKSEL TMR0
+    CLRF    TMR0
+    MOVLW   .248
+    MOVWF   TMR0
+    
+    RETURN
+    
+;*******************************************************************************
+; DELAY
+;******************************************************************************* 
+    
+DELAY   
+	MOVLW .17
+	MOVWF CONT2
+CONFIG1:	
+	MOVLW .100
+	MOVWF CONT1
+RESTA2:    
+	DECFSZ	CONT1, F
+	GOTO	RESTA2
+	DECFSZ	CONT2, F
+	GOTO	CONFIG1
+	RETURN
+    
+ END
+
--- dev_test/dev_test_v1.py
+++ dev_test/dev_test_v1.py
@@ -9,10 +9,14 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import openpyxl as xl
 
-src_path = r"C:/Users/Peter/Documents/GitHub/tests/dev_test/"
-xls_path = r"C:/Users/Peter/Documents/GitHub/tests/dev_test/Processed/"
-nonxls_path = r"C:/Users/Peter/Documents/GitHub/tests/dev_test/Not_applicable/"
-directory = "."# "C:/Users/Peter/Documents/GitHub/tests/dev_test"
+dir = os.path.dirname(__file__)
+xls_path = os.path.join(dir,'Processed\\')
+
+# Cambiar los paths para hacer pruebas en otras computadoras
+src_path = r"C:/Users/Peter/Documents/GitHub/tests/dev_test/"					# Directorio donde se van a "crear" los archivos
+#xls_path = r"C:/Users/Peter/Documents/GitHub/tests/dev_test/Processed/"			# Directorio donde se van a mover los archivos .xls*
+nonxls_path = r"C:/Users/Peter/Documents/GitHub/tests/dev_test/Not_applicable/"	# Directorio donde se van a mover los archivos != .xls*
+directory = "."
 
 class watcher:			# Inicialización de la clase "watcher", la cual establece las instancias con los argumentos ingresados.
 	def __init__(self, directory = directory, handler = FileSystemEventHandler()):	# Argumentos:
@@ -38,6 +42,28 @@ class myHandler(FileSystemEventHandler):
 					if fnmatch.fnmatch(file, "Master.xls*"):
 						pass
 					else:
+						# -------------- Copiando valores de celda de un workbook a otro -------------
+						src_wb = xl.load_workbook(src_path+file)
+						src_sheet_num = len(src_wb.sheetnames)
+						dest_wb = xl.load_workbook(src_path+"Master.xlsx")
+						dest_sheet_num = len(dest_wb.sheetnames)
+						print("sheet_num: "+ str(src_sheet_num))
+						print("dest_sheet_num: "+ str(dest_sheet_num))
+						current_sheet = 0
+						while current_sheet < src_sheet_num:
+							src_ws = src_wb.worksheets[current_sheet]
+							dest_ws = dest_wb.worksheets[dest_sheet_num-1]
+							src_rows = src_ws.max_row
+							src_cols = src_ws.max_column
+							for i in range(1, src_rows + 1):
+								for j in range(1, src_cols + 1):
+									cells = src_ws.cell(row = i, column = j)
+									dest_ws.cell(row = i, column = j).value = cells.value
+							dest_sheet_num = dest_sheet_num + 1
+							dest_wb.create_sheet("Sheet"+str(dest_sheet_num))
+							current_sheet = current_sheet + 1
+						dest_wb.save(str(src_path+"Master.xlsx"))
+						# ----------------------------------------------------------------------------
 						print(file+" has been processed")
 						shutil.move(src_path+file, xls_path+file)
 				elif not fnmatch.fnmatch(file, "*.xls*"):
